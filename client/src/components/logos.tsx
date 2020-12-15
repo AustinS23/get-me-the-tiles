dc97a12a41e71b3b0a6bb5b94d7c12351417d89b
AustinS23 <austin.shen@pushpay.com>
2020-12-15 13:06:09 +1300

    Display the logo and the name

--- client/src/components/logos.tsx
+++ client/src/components/logos.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTilesQuery } from "../graphql/_generated";
+import './logos.css';
 
 const Logos = () => {
   const { loading, data, error } = useTilesQuery();
@@ -9,13 +10,16 @@ const Logos = () => {
   }
 
   if (error || !data) {
-    return <div>error: {error?.message || 'no data'}</div>;
+    return <div>error: {error?.message || "no data"}</div>;
   }
 
   return (
-    <div>
-      {data.tiles.map(({ id }) => (
-        <div key={id}>{id}</div>
+    <div className="container">
+      {data.tiles.map(({ id, attributes: { currentTileUrl, name } }) => (
+        <div className="logo-container" key={id} >
+          <img className="logo" src={currentTileUrl!} alt={`logo for ${name}`} />
+          <div className="name">{name}</div>
+        </div>
       ))}
     </div>
   );
